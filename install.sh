a6d38a3d7c9d06c5ae121888aa1d8e19b00e6f9e
Mnvim <167700679+Mnvim@users.noreply.github.com>
2024-06-03 17:48:26 -0300

    install.sh picom pijulius

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev lib
 
 # Dependencias de Picom
 
-sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
+sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev libconfig-dev libdbus-1-dev libegl-dev libgl-dev libepoxy-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev ninja-build
 
 # Instalamos paquetes adionales
 
@@ -30,7 +30,7 @@ mkdir ~/github
 
 cd ~/github
 git clone --recursive https://github.com/polybar/polybar
-git clone https://github.com/ibhagwan/picom.git
+git clone https://github.com/pijulius/picom.git
 
 # Instalando Polybar
 
@@ -44,8 +44,7 @@ sudo make install
 # Instalando Picom
 
 cd ~/github/picom
-git submodule update --init --recursive
-meson --buildtype=release . build
+meson setup --buildtype=release build
 ninja -C build
 sudo ninja -C build install
 
